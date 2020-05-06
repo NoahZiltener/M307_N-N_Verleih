99b1a307a74e9c50ce4e6d139edd9b5682be933f
Nick <nick7@hotmail.ch>
2020-05-06 10:56:16 +0200

    updated Loan Model

--- app/Controllers/WelcomeController.php
+++ app/Controllers/WelcomeController.php
@@ -1,2 +1,11 @@
 <?php
-require 'app/Views/welcome.view.php';
+    require_once "app/Models/Loan.php";
+
+    if($_SERVER['REQUEST_METHOD']=== 'POST'){
+        foreach($_POST as &$loanId){
+            
+        }
+    }
+
+
+    require 'app/Views/welcome.view.php';
--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once "app/Models/Movie.php";
+
 class Loan
 {
     public $loanid;
@@ -80,10 +82,10 @@ class Loan
         $statement->bindParam(':lastname', $this->lastname, PDO::PARAM_STR);
         $statement->bindParam(':phone', $this->phone, PDO::PARAM_STR);
         $statement->bindParam(':email', $this->email, PDO::PARAM_STR);
-        $statement->bindParam(':loandate', $this->countOfRates, PDO::PARAM_INT);
-        $statement->bindParam(':returndate', $this->deadline, PDO::PARAM_STR);
-        $statement->bindParam(':returned', $this->fk_creditdealsId, PDO::PARAM_BOOL);
-        $statement->bindParam(':fk_movie', $this->fk_statusId, PDO::PARAM_INT);
+        $statement->bindParam(':loandate', $this->loanDate, PDO::PARAM_INT);
+        $statement->bindParam(':returndate', $this->returnDate, PDO::PARAM_STR);
+        $statement->bindParam(':returned', $this->returned, PDO::PARAM_BOOL);
+        $statement->bindParam(':fk_movie', $this->movie->movieid, PDO::PARAM_INT);
 
         return $statement->execute();
     }
