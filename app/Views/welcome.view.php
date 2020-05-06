4a479d7e933d7195b692965ee03a99746e042cab
Nick <nick7@hotmail.ch>
2020-05-06 10:03:20 +0200

    startet with WelcomeView

--- app/Views/welcome.view.php
+++ app/Views/welcome.view.php
@@ -1,14 +1,31 @@
 <!DOCTYPE html>
 <html lang="de">
+
 <head>
     <meta charset="UTF-8">
-    <title>Welcome Seite</title>
+    <title>Vidicted</title>
     <link rel="stylesheet" href="public/css/app.css">
+    <script src="public/js/app.js"></script>
 </head>
+
 <body>
 
-<h1 class="welcome">Willkommen</h1>
+    <h1>Home</h1>
 
-<script src="public/js/app.js"></script>
+    <h2>Videotheke</h2>
+
+    <table>
+        <tr>
+            <th class="tableTitle">Name</th>
+            <th class="tableTitle">Video</th>
+            <th class="tableTitle">Ausleihfrist</th>
+            <th class="tableTitle">Ausleihstatus</th>
+            <th class="tableTitle">Bearbeiten</th>
+        </tr>
+    </table>
+
+    <a href="create"><button>Neue Ausleihe</button></a>
+
 </body>
+
 </html>
--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,1 @@
 // Javascript
-console.info('JS geladen.');
